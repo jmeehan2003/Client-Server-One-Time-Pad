c166a063dc4a72c2513fda637d49764a51a2353c
James Meehan <ghoyas2003@gmail.com>
2018-06-06 20:30:31 -0400

    add random key generator

---
+++ keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		fprintf(stderr, "Error starting program\n"); 
+		fprintf(stderr, "Usage: keygen [length of key file]\n");
+		exit(1);
+	}
+
+	// use srand to get different random results each time
+	srand(time(NULL));
+
+	// convert user argument to an integer
+	int size = atoi(argv[1]);
+
+	int counter = 0;
+	char letter;
+
+	// get a random letter and print it to stdout until key file size has been reached
+	while (counter < size)
+	{
+		// get random letter
+		letter = (rand() % 27) + 65;
+
+		// make adjustment for space character
+		if (letter == 91)
+			letter = 32;
+
+		// print to stdout
+		printf("%c", letter);
+
+		// increment counter
+		counter++;
+	}
+
+	// add newline at end
+	printf("\n");
+
+	return 0;
+}
